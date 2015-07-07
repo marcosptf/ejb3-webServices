6fd4adc73d9d708634690567ac4362910c9bad1d
marcosptf <marcosptf@yahoo.com.br>
2015-07-07 17:58:28 -0300

    Update HelloUserClient.java

--- chapter1/src/ejb/client/ejb3inaction/example/HelloUserClient.java
+++ chapter1/src/ejb/client/ejb3inaction/example/HelloUserClient.java
@@ -1,27 +1,24 @@
+
 package ejb3inaction.example;
 
 import javax.ejb.EJB;
 import javax.naming.*;
 
-public class HelloUserClient {
+public class HelloUserClient{
       
-        private static HelloUser helloUser;
+      private static HelloUser helloUser;
 
-    	public  static void main(String[] args) {
-             System.out.println("Invoking EJB");     
+    	public static void main(String[] args) {
+    	      
+          System.out.println("Invoking EJB");     
 
-             try
-             {
+          try {
               Context ctx = new InitialContext();
               helloUser = (HelloUser) ctx.lookup("HelloUser#ejb3inaction.example.HelloUser");
-     
-             
-             helloUser.sayHello("Curious George");
-             System.out.println("Invoked EJB successfully .. see server console for output");     
-            }
-            catch (Exception e)
-            {
+              helloUser.sayHello("Curious George");
+              System.out.println("Invoked EJB successfully .. see server console for output");     
+          } catch (Exception e) {
               e.printStackTrace();
-             }
-}
+          }
+      }
 }
