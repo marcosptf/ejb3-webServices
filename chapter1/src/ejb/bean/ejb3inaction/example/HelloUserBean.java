663c7abffcc5be38d121db17cabc252527d70b42
marcosptf <marcosptf@yahoo.com.br>
2015-07-07 17:52:40 -0300

    Update HelloUserBean.java

--- chapter1/src/ejb/bean/ejb3inaction/example/HelloUserBean.java
+++ chapter1/src/ejb/bean/ejb3inaction/example/HelloUserBean.java
@@ -1,8 +1,13 @@
+
 package ejb3inaction.example;
+
 import javax.ejb.Stateless;
+
 @Stateless(mappedName="HelloUser")                                                          
 public class HelloUserBean implements HelloUser {                   
+    
     public void sayHello(String name) {
         System.out.println("Hello " + name + " welcome to EJB 3 In Action!");
     }
+    
 }
